13da2a34b12c17c5ef721e265ba7506a16fe7f3c
Mathias-Kodehode <mathias.kodehode@gmail.com>
2025-02-12 09:44:09 +0100

    added top and bottom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Header />
+    <Footer />
+  </StrictMode>
+);
