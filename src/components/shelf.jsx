cc6ab73a21dc4e370170c0bb0c0ca9532c34b5b9
Gabriel-Kodehode <gabriel.kodehode@gmail.com>
2025-02-12 11:07:08 +0100

    api test 0.6

--- src/components/shelf.jsx
+++ src/components/shelf.jsx
@@ -1,9 +1,45 @@
 import "./shelf.css";
+import BookList from "../App.jsx";
+
+// const Projekts = BookList
+
+// export default function ProjektDisplay() {
+//   return (
+//     <div id="ProjektsBox">
+//       {Projekts.map((projekt, index) => (
+//         <div id="ProjektFile">
+//           <h3>{projekt.name}</h3>
+//           <p>{projekt.beskrivelse}</p>
+//           <button>
+//             <a href={projekt.link} target="_blank">
+//               Besøk prosjektet
+//             </a>
+//           </button>
+//           <img src={projekt.image} alt={projekt.name} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+const Projekts = BookList;
 
 function Shelf() {
   return (
     <>
       <div className="topShelf">
+        {Projekts.map((projekt, index) => (
+          <div id="ProjektFile">
+            <h3>{projekt.name}</h3>
+            <p>{projekt.beskrivelse}</p>
+            <button>
+              <a href={projekt.link} target="_blank">
+                Besøk prosjektet
+              </a>
+            </button>
+            <img src={projekt.image} alt={projekt.name} />
+          </div>
+        ))}
         <img src="/public/shelf.png" alt="" />
       </div>
       <div className="shelf">
