cc835d25178d5e0a16b54940f9561f916719455b
Mathias-Kodehode <mathias.kodehode@gmail.com>
2025-02-12 10:25:17 +0100

    added books

---
+++ src/components/shelf.jsx
@@ -0,0 +1,49 @@
+import "./shelf.css";
+
+function Shelf() {
+  return (
+    <>
+      <div className="topShelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+      <div className="shelf">
+        <img src="/public/shelf.png" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default Shelf;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
+import Shelf from "./components/shelf.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Header />
+    <Shelf />
     <Footer />
   </StrictMode>
 );
