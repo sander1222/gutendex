865960d156689294c0fab5e3b7b86f227cd91be6
Mathias-Kodehode <mathias.kodehode@gmail.com>
2025-02-12 11:06:07 +0100

    search

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,11 +2,10 @@ import "./header.css";
 
 function Header() {
   return (
-    <>
-      <header className="top">
-        <img src="/public/Top.png" alt="" />
-      </header>
-    </>
+    <header className="top">
+      <img src="/public/Top.png" alt="" />
+      <input type="text" placeholder="Search..." className="search-bar" />
+    </header>
   );
 }
 
