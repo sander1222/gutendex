4e5ae0f8866b94ceb80dd6f44fbc93a1d41dd42f
sander1222 <sander122.kodehode@gmail.com>
2025-02-12 10:10:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
 
 
-function App() {
-  const [count, setCount] = useState(0);
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-  return <></>;
+const categories = [
+  "Fiction",
+  "Myestery",
+  "Thriller",
+  "Romance",
+  "Fantasy",
+  "Morality",
+  "Society",
+  "Power",
+  "Justice",
+  "Adventure",
+  "Tragedy",
+  "War",
+  "Philosophy",
+];
+
+export default function BookList() {
+  const [books, setBooks] = useState([]);
+  const [category, setCategory] = useState("Fiction");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    console.log(`Fetching books for category: ${category}, page: ${page}`);
+
+    fetch(`https://gutendex.com/books/?topic=${category}&page=${page}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("API response:", data);
+        setBooks(data.results || []);
+        setTotalPages(Math.ceil((data.count || 1) / 10));
+      })
+      .catch((error) => console.error("Error fetching books:", error));
+  }, [category, page]);
 }
-
-export default App;
