da95b5f393e4adab7dae424eaed22efebd25194e
Gabriel-Kodehode <gabriel.kodehode@gmail.com>
2025-02-12 10:16:04 +0100

    Bok site

--- src/Bok.jsx
+++ src/Bok.jsx
@@ -1,12 +1,10 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import Bok from "./components/bok.jsx";
-// import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./Bok.css";
+import Bok from "./components/bok.jsx";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//     <Bok />
-//   </StrictMode>
-// );
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Bok />
+  </StrictMode>
+);
